4daf6801c42c84c7bdc04b40fa1f5c18f6ea750c
Sufyan Ali <msufyan.202105869@gcuf.edu.pk>
2026-06-10 18:33:12 +0500

    feat: create Portfolio Mongoose model for storing user profiles and project details

---
+++ src/models/portfolio.model.js
@@ -0,0 +1,90 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const portfolioSchema = new Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    role: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    bio: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    about: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    skills: {
+        type: [String],
+        required: true
+    },
+    projects: [
+        {
+            title: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            description: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            image: {
+                type: String,
+                required: true
+            },
+            tags: {
+                type: [String],
+                required: true
+            },
+            codeLink: {
+                type: String,
+                required: true
+            },
+            liveLink: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    email: {
+        type: String,
+        required: true
+    },
+    github: {
+        type: String,
+        required: true
+    },
+    linkedin: {
+        type: String,
+        required: true
+    },
+    whatsapp: {
+        type: String,
+        required: true
+    }
+    
+}, { timestamps: true });
+
+const Portfolio = mongoose.model('Portfolio', portfolioSchema);
+export default Portfolio;
