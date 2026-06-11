50309dbdfee597f4c78d9f9e3612fba57216f8be
Sufyan Ali <msufyan.202105869@gcuf.edu.pk>
2026-06-11 07:37:36 +0500

    feat: add portfolio creation route with file upload middleware

--- src/routes/portfolio.routes.js
+++ src/routes/portfolio.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import { createPortfolio } from "../controllers/portfolio.controller.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
 
 const portfolioRouter = Router();
 
-portfolioRouter.post('/create', upload.fields([
+portfolioRouter.post('/create', authMiddleware, upload.fields([
     { name: 'image', maxCount: 1 },
-    { name: 'projects', maxCount: 1 }
+    { name: 'projectImages', maxCount: 15 }
 ]), createPortfolio);
 
 export default portfolioRouter;
