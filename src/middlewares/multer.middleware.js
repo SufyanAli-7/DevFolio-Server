dce064384beec1da94e902d4568379c2bddfedb5
Sufyan Ali <msufyan.202105869@gcuf.edu.pk>
2026-06-17 15:53:25 +0500

    feat: add multer middleware for temporary file storage configuration

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,12 +1,13 @@
 import multer from "multer";
+import os from "os";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+      cb(null, os.tmpdir())
     },
     filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname)
     }
   })
   
